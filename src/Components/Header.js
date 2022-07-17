d3dd0b4cf3598bcb8d0f352d1c12ff6ca17e4f7f
hodaeg <73181987+hodaeg@users.noreply.github.com>
2022-07-16 22:26:47 -0500

    mobile nav

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,14 @@
 import react from 'react'
 import rclogo from '../rclogo.png'
 import { Link } from 'react-router-dom'
+import { hamburger } from '../bars-solid.svg'
 
 export default function Header () {
     return (
         <nav>
             <div className="nav-rc">
                 <Link to ="/"><img src={rclogo} className="logo"/></Link>
+                <img src ={hamburger}></img>
                 <div className="nav-items-container">
                     <ul className="nav-items">
                         <button><Link to ="/submitsong">Submit Song</Link></button>
